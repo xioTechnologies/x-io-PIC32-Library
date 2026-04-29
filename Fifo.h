f48a8d826e115e4f62aa01acf6516ec207483eb9
x-io Technologies <social@x-io.co.uk>
2026-04-29 08:17:45 +0100

    Add packet read/write to FIFO

--- Fifo.h
+++ Fifo.h
@@ -69,6 +69,56 @@ static inline __attribute__((always_inline)) size_t FifoAvailableRead(Fifo * con
 }
 
 /**
+ * @brief Reads data from the FIFO.
+ * @param fifo FIFO structure.
+ * @param destination Destination.
+ * @param numberOfBytes Number of bytes.
+ * @return Number of bytes read.
+ */
+static inline __attribute__((always_inline)) size_t FifoRead(Fifo * const fifo, void* const destination, size_t numberOfBytes) {
+
+    // Do nothing if no bytes available to read
+    const size_t bytesAvailable = FifoAvailableRead(fifo);
+    if (bytesAvailable == 0) {
+        return 0;
+    }
+
+    // Limit number of bytes to number available
+    if (numberOfBytes > bytesAvailable) {
+        numberOfBytes = bytesAvailable;
+    }
+
+    // Read data with no wraparound
+    if ((fifo->readIndex + numberOfBytes) < fifo->dataSize) {
+        memcpy(destination, (void*) &fifo->data[fifo->readIndex], numberOfBytes);
+        fifo->readIndex += numberOfBytes;
+        return numberOfBytes;
+    }
+
+    // Read data with wraparound
+    const size_t numberOfBytesBeforeWraparound = fifo->dataSize - fifo->readIndex;
+    memcpy(destination, (void*) &fifo->data[fifo->readIndex], numberOfBytesBeforeWraparound);
+    const size_t numberOfBytesAfterWraparound = numberOfBytes - numberOfBytesBeforeWraparound;
+    memcpy(&((uint8_t*) destination)[numberOfBytesBeforeWraparound], (void*) fifo->data, numberOfBytesAfterWraparound);
+    fifo->readIndex = numberOfBytesAfterWraparound;
+    return numberOfBytes;
+}
+
+/**
+ * @brief Reads a byte from the FIFO. This function must only be called if
+ * there are bytes available to read.
+ * @param fifo FIFO structure.
+ * @return Byte.
+ */
+static inline __attribute__((always_inline)) uint8_t FifoReadByte(Fifo * const fifo) {
+    const uint8_t byte = fifo->data[fifo->readIndex];
+    if (++fifo->readIndex >= fifo->dataSize) {
+        fifo->readIndex = 0;
+    }
+    return byte;
+}
+
+/**
  * @brief Provides a pointer to the next contiguous block of data in the FIFO.
  * FifoReadPointerComplete must be called after data has been read.
  * @param fifo FIFO structure.
@@ -98,51 +148,45 @@ static inline __attribute__((always_inline)) void FifoReadPointerComplete(Fifo *
 }
 
 /**
- * @brief Reads data from the FIFO.
+ * @brief Returns the size of the next packet in the FIFO. This function must
+ * only be used if all data in the FIFO was written using FifoWritePacket.
  * @param fifo FIFO structure.
- * @param destination Destination.
- * @param numberOfBytes Number of bytes.
- * @return Number of bytes read.
+ * @return Size of the next packet.
  */
-static inline __attribute__((always_inline)) size_t FifoRead(Fifo * const fifo, void* const destination, size_t numberOfBytes) {
-
-    // Do nothing if no bytes available to read
-    const size_t bytesAvailable = FifoAvailableRead(fifo);
-    if (bytesAvailable == 0) {
+static inline __attribute__((always_inline)) size_t FifoAvailableReadPacket(Fifo * const fifo) {
+    if (FifoAvailableRead(fifo) == 0) {
         return 0;
     }
-
-    // Limit number of bytes to number available
-    if (numberOfBytes > bytesAvailable) {
-        numberOfBytes = bytesAvailable;
+    if (fifo->readIndex == fifo->dataSize - 1) {
+        fifo->readIndex = 0; // packetSize will never be written to wrapround
     }
-
-    // Read data
-    if ((fifo->readIndex + numberOfBytes) >= fifo->dataSize) {
-        const size_t numberOfBytesBeforeWraparound = fifo->dataSize - fifo->readIndex;
-        memcpy(destination, (void*) &fifo->data[fifo->readIndex], numberOfBytesBeforeWraparound);
-        const size_t numberOfBytesAfterWraparound = numberOfBytes - numberOfBytesBeforeWraparound;
-        memcpy(&((uint8_t*) destination)[numberOfBytesBeforeWraparound], (void*) fifo->data, numberOfBytesAfterWraparound);
-        fifo->readIndex = numberOfBytesAfterWraparound;
-    } else {
-        memcpy(destination, (void*) &fifo->data[fifo->readIndex], numberOfBytes);
-        fifo->readIndex += numberOfBytes;
-    }
-    return numberOfBytes;
+    volatile void *unaligned;
+    size_t numberOfBytes __attribute__((unused));
+    FifoReadPointer(fifo, &unaligned, &numberOfBytes); // FifoReadPacket will call FifoReadPointerComplete when data is read
+    uint16_t packetSize;
+    memcpy(&packetSize, (void*) unaligned, sizeof (packetSize)); // uint16_t cannot be dereferenced from an odd address
+    return packetSize;
 }
 
 /**
- * @brief Reads a byte from the FIFO. This function must only be called if
- * there are bytes available to read.
+ * @brief Reads a packet from the FIFO. This function must only be used if all
+ * data in the FIFO was written using FifoWritePacket.
  * @param fifo FIFO structure.
- * @return Byte.
+ * @param destination Destination.
+ * @param destinationSize Destination size.
+ * @return Size of the packet.
  */
-static inline __attribute__((always_inline)) uint8_t FifoReadByte(Fifo * const fifo) {
-    const uint8_t byte = fifo->data[fifo->readIndex];
-    if (++fifo->readIndex >= fifo->dataSize) {
-        fifo->readIndex = 0;
+static inline __attribute__((always_inline)) size_t FifoReadPacket(Fifo * const fifo, void* const destination, const size_t destinationSize) {
+    const size_t packetSize = FifoAvailableReadPacket(fifo);
+    if (packetSize == 0) {
+        return 0;
     }
-    return byte;
+    if (packetSize > destinationSize) {
+        return 0;
+    }
+    FifoReadPointerComplete(fifo, sizeof (uint16_t));
+    FifoRead(fifo, destination, packetSize);
+    return packetSize;
 }
 
 /**
@@ -173,17 +217,19 @@ static inline __attribute__((always_inline)) FifoResult FifoWrite(Fifo * const f
         return FifoResultError;
     }
 
-    // Write data
-    if ((fifo->writeIndex + numberOfBytes) >= fifo->dataSize) {
-        const size_t numberOfBytesBeforeWraparound = fifo->dataSize - fifo->writeIndex;
-        memcpy((void*) &fifo->data[fifo->writeIndex], data, numberOfBytesBeforeWraparound);
-        const size_t numberOfBytesAfterWraparound = numberOfBytes - numberOfBytesBeforeWraparound;
-        memcpy((void*) fifo->data, &((uint8_t*) data)[numberOfBytesBeforeWraparound], numberOfBytesAfterWraparound);
-        fifo->writeIndex = numberOfBytesAfterWraparound;
-    } else {
+    // Write data with no wraparound
+    if ((fifo->writeIndex + numberOfBytes) < fifo->dataSize) {
         memcpy((void*) &fifo->data[fifo->writeIndex], data, numberOfBytes);
         fifo->writeIndex += numberOfBytes;
+        return FifoResultOk;
     }
+
+    // Write data with wraparound
+    const size_t numberOfBytesBeforeWraparound = fifo->dataSize - fifo->writeIndex;
+    memcpy((void*) &fifo->data[fifo->writeIndex], data, numberOfBytesBeforeWraparound);
+    const size_t numberOfBytesAfterWraparound = numberOfBytes - numberOfBytesBeforeWraparound;
+    memcpy((void*) fifo->data, &((uint8_t*) data)[numberOfBytesBeforeWraparound], numberOfBytesAfterWraparound);
+    fifo->writeIndex = numberOfBytesAfterWraparound;
     return FifoResultOk;
 }
 
@@ -205,6 +251,43 @@ static inline __attribute__((always_inline)) FifoResult FifoWriteByte(Fifo * con
     if (++fifo->writeIndex >= fifo->dataSize) {
         fifo->writeIndex = 0;
     }
+    return FifoResultOk;
+}
+
+/**
+ * @brief Returns the space available to write a packet to the FIFO.
+ * @param fifo FIFO structure.
+ * @return Space available for packet payload.
+ */
+static inline __attribute__((always_inline)) size_t FifoAvailableWritePacket(Fifo * const fifo) {
+    const size_t available = FifoAvailableWrite(fifo);
+    const size_t overhead = sizeof (uint16_t) + 1; // includes extra byte in event of wraparound
+    if (available <= overhead) {
+        return 0;
+    }
+    return available - overhead;
+}
+
+/**
+ * @brief Writes a packet to the FIFO.
+ * @param fifo FIFO structure.
+ * @param data Data.
+ * @param numberOfBytes Number of bytes.
+ * @return Result.
+ */
+static inline __attribute__((always_inline)) FifoResult FifoWritePacket(Fifo * const fifo, const void* const data, const size_t numberOfBytes) {
+    if (numberOfBytes > UINT16_MAX) {
+        return FifoResultError;
+    }
+    if (numberOfBytes > FifoAvailableWritePacket(fifo)) {
+        return FifoResultError;
+    }
+    if (fifo->writeIndex == fifo->dataSize - 1) {
+        fifo->writeIndex = 0; // packetSize must not be written to wrapround
+    }
+    const uint16_t packetSize = (uint16_t) numberOfBytes;
+    FifoWrite(fifo, &packetSize, sizeof (packetSize));
+    FifoWrite(fifo, data, numberOfBytes);
     return FifoResultOk;
 }
 
